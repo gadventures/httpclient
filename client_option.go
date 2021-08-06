5e59e1785fd4ae993ae1985b0068922a38088d95
Ammaar Esmailjee <ammaar.esmailjee@gmail.com>
2021-08-06 02:16:42 -0400

    update client Option comments

--- client_option.go
+++ client_option.go
@@ -10,7 +10,7 @@ import (
 // see: https://sagikazarmark.hu/blog/functional-options-on-steroids/
 type Option func(*client) error
 
-// DialTimeout is configuration option to pass to Client it changes how long
+// DialTimeout is configuration option to pass to client it changes how long
 // the client will wait to establish the TCP connection
 func DialTimeout(t time.Duration) Option {
 	return func(c *client) error {
@@ -19,9 +19,8 @@ func DialTimeout(t time.Duration) Option {
 	}
 }
 
-// DisableHTTP2 is configuration option to pass to Client
-// it disables the transparent support for HTTP/2
-// thereby forcing HTTP/1.1
+// DisableHTTP2 is configuration option to pass to client. It disables the
+// Transport support for HTTP/2, thereby forcing HTTP/1.1.
 func DisableHTTP2() Option {
 	return func(c *client) error {
 		c.disableHTTP2 = true
@@ -29,7 +28,7 @@ func DisableHTTP2() Option {
 	}
 }
 
-// DisableKeepAlive is configuration option to pass to Client
+// DisableKeepAlive is configuration option to pass to client
 // it disables KeepAlive for the tcp connection
 func DisableKeepAlive() Option {
 	return func(c *client) error {
@@ -38,10 +37,10 @@ func DisableKeepAlive() Option {
 	}
 }
 
-// Headers is configuration option to pass headers to Client
-// it makes GET requests use the provided headers
-// use this for headers shared among all requests
-// for request specific headers use the ExtraHeaders RequestOption
+// Headers is configuration option to pass headers to Client. It makes GET
+// requests using the provided headers. Use this for headers that are to be
+// shared among all Requests. For Request specific headers use the
+// SetHeaders RequestOption.
 func Headers(headers http.Header) Option {
 	return func(c *client) error {
 		for k, v := range headers {
@@ -51,10 +50,9 @@ func Headers(headers http.Header) Option {
 	}
 }
 
-// IdleConnTimeout is a configuration option to pass to Client
-// it sets how long will idle connections live in a pool
-// waiting to be used again.
-// Once the timeout is reached connections are closed and removed from pool.
+// IdleConnTimeout is a configuration option to pass to client. It sets how
+// long idle connections waiting to be used again live in a pool. Once the
+// timeout is reached connections are closed and removed from pool.
 func IdleConnTimeout(t time.Duration) Option {
 	return func(c *client) error {
 		c.idleConnTimeout = t
@@ -62,12 +60,13 @@ func IdleConnTimeout(t time.Duration) Option {
 	}
 }
 
-// KeepAliveTimeout is configuration option to pass to Client
-// it changes the timeout for how long the tcp connection stays open
-// After which period will tcp keep alive happen on the TCP connection
-// safe to leave the default but can be tweaked should one notice
-// connections being reset by peers sooner than expected
-// Note: OS may override this value
+// KeepAliveTimeout is configuration option to pass to client. It changes the
+// timeout for how long the TCP Connection stays open, after which period will
+// TCP keep alive happen on the TCP connection. It is safe to leave the default
+// value, but can be tweaked should one notice connections being reset by peers
+// sooner than expected.
+//
+// NOTE: The OS may override this value
 func KeepAliveTimeout(t time.Duration) Option {
 	return func(c *client) error {
 		c.keepAliveTimeout = t
@@ -75,9 +74,8 @@ func KeepAliveTimeout(t time.Duration) Option {
 	}
 }
 
-// Logger is configuration option to pass to Client
-// it changes where the debug info is written
-// (ioutil.Discard by default)
+// Logger is configuration option to pass to client. It changes where the debug
+// logs are written (ioutil.Discard by default).
 func Logger(w io.Writer) Option {
 	return func(c *client) error {
 		c.logWriter = w
@@ -85,9 +83,9 @@ func Logger(w io.Writer) Option {
 	}
 }
 
-// LogPrefix is configuration option to pass to Client
-// to change the prefix used in Clients log output.
-// This can be useful when one is using several httpclients.
+// LogPrefix is configuration option to pass to client. It will change the
+// prefix used in Client's log output. This can be useful when one is using
+// several httpclients
 func LogPrefix(p string) Option {
 	return func(c *client) error {
 		c.logPrefix = p
@@ -95,9 +93,8 @@ func LogPrefix(p string) Option {
 	}
 }
 
-// MaxIdleConns is configuration option to pass to Client
-// it changes the maximum idle connections that the client will keep
-// in a pool
+// MaxIdleConns is configuration option to pass to client. It changes the
+// maximum idle connections that the client will keep in a pool.
 func MaxIdleConns(n int) Option {
 	return func(c *client) error {
 		if n < 0 {
@@ -108,10 +105,9 @@ func MaxIdleConns(n int) Option {
 	}
 }
 
-// MaxIdleConnsPerHost is configuration option to pass to Client
-// it changes the maximum idle connections that the client will keep in a pool
-// for a single host.
-// If not specified this will be the same as MaxIdleConns.
+// MaxIdleConnsPerHost is configuration option to pass to client. It changes
+// the maximum idle connections that the client will keep in a pool for a
+// single host. If not specified this will be the same as MaxIdleConns.
 func MaxIdleConnsPerHost(n int) Option {
 	return func(c *client) error {
 		if n < 0 {
@@ -122,10 +118,9 @@ func MaxIdleConnsPerHost(n int) Option {
 	}
 }
 
-// RedirectPolicy is configuration option to pass to Client
-// it changes what client does on redirects
-// the default behaviour is to copy the headers from original
-// request and try again up to 10 times
+// RedirectPolicy is configuration option to pass to client. It changes what
+// the client does on redirects. The default behaviour is to copy the original
+// request headers and try again up to 10 times.
 func RedirectPolicy(redirectFunc func(req *http.Request, via []*http.Request) error) Option {
 	return func(c *client) error {
 		c.redirectFunc = redirectFunc
@@ -133,8 +128,8 @@ func RedirectPolicy(redirectFunc func(req *http.Request, via []*http.Request) er
 	}
 }
 
-// TLSHandshakeTimeout is a configuration option to pass to Client
-// it limits the time spent performing the TLS handshake.
+// TLSHandshakeTimeout is a configuration option to pass to client. It limits
+// the time spent performing the TLS handshake.
 func TLSHandshakeTimeout(t time.Duration) Option {
 	return func(c *client) error {
 		c.tlsHandshakeTimeout = t
@@ -142,8 +137,8 @@ func TLSHandshakeTimeout(t time.Duration) Option {
 	}
 }
 
-// ResponseHeaderTimeout is a configuration option to pass to Client
-// it limits the time spent reading the headers of the response.
+// ResponseHeaderTimeout is a configuration option to pass to client. It limits
+// the time spent reading the headers of the response.
 func ResponseHeaderTimeout(t time.Duration) Option {
 	return func(c *client) error {
 		c.responseHeaderTimeout = t
@@ -151,27 +146,27 @@ func ResponseHeaderTimeout(t time.Duration) Option {
 	}
 }
 
-// WithRoundTripper is configuration option to pass to Client
-// this will change the http.RoundTripper that the client will use
-// note use of this renders the use of httpclient pointless
-// since if you are managing your own transports
-// you might as well use net/http directly
-// however this option is useful for unittests
-// e.g. when using httpmock to verify expected traffic
-// when testing some code that uses httpclient
+// RoundTripperFunc is like http.HandlerFunc, but for RoundTripper interface.
+type RoundTripperFunc func(*http.Request) (*http.Response, error)
+
+// RoundTrip satisfies the http.RoundTripper interface.
+func (r RoundTripperFunc) RoundTrip(req *http.Request) (*http.Response, error) {
+	return r(req)
+}
+
+// WithRoundTripper is configuration option to pass to client. This will change
+// the http.RoundTripper that the client will use.
+//
+// NOTE: the usage of this renders the use of httpclient pointless, because if
+//       you are managing your own transports, you might as well use net/http
+//       directly. This option is useful for unittests, such as when using
+//       httpmock to verify expected traffic when testing some code that uses
+//       httpclient.
 func WithRoundTripper(rt http.RoundTripper) Option {
 	return func(c *client) error {
 		c.customRoundTripper = rt
 		return nil
 	}
-}
-
-// RoundTripperFunc is like http.HandlerFunc, but for RoundTrip
-type RoundTripperFunc func(*http.Request) (*http.Response, error)
-
-// RoundTrip is the http.RoundTripper implementation
-func (r RoundTripperFunc) RoundTrip(req *http.Request) (*http.Response, error) {
-	return r(req)
 }
 
 // set the Options provided to the New method

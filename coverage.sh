8f5e31d794aeced83c2d3333e70f91395ae25cd8
Ammaar Esmailjee <ammaar.esmailjee@gmail.com>
2021-09-02 17:17:20 -0400

    Update coverage.sh
    Makes coverage.sh a bit friendlier to use
    
    Example usage:
    
    $> ./coverage.sh
    * uses "coverage.out" for the coverprofile output
    * runs go tool cover -func
    
    $> OUT=file.out ./coverage.sh html
    * uses "file.out" for the coverprofile output
    * runs go tool cover -html

--- coverage.sh
+++ coverage.sh
@@ -1,8 +1,32 @@
-#/bin/sh
-rm coverage.out
-go test -coverprofile=coverage.out -race -v
-if [ -z "$1" ]; then
-  go tool cover -func=coverage.out
-else
-  go tool cover -html=coverage.out
-fi
+#!/usr/bin/env bash
+
+set -eu
+
+function usage() {
+  echo "Usage: [OUT=filename] $0 [html|func]"
+  exit 1
+}
+
+# OUT={filename}
+: "${OUT:=coverage.out}"
+
+# COVER default to func
+COVER="${1:-func}"
+
+# delete out file, ignoring if it exists
+rm -f "$OUT"
+
+# run tests with coverprofile
+go test -v -race -coverprofile=$OUT
+
+case "$COVER" in
+  func)
+    go tool cover -func=$OUT
+    ;;
+  html)
+    go tool cover -html=$OUT
+    ;;
+  *)
+    usage
+    ;;
+esac
